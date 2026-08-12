a6f0803291708ff4300c1c7a0da18caf59e69f8e
naplaukcuytin-cyber <phongconcac207@gmail.com>
2026-08-13 03:21:54 +0700

    Update config.js

--- app/Model/config.js
+++ app/Model/config.js
@@ -1,3 +1,7 @@
-let config = {};
-
-module.exports = config;
+module.exports = {
+  host: 'bc3fesmygiz7qvs23-mysql.services.clever-cloud.com',
+  user: 'uvcuhldw9vxoipts',
+  password: '41JwH6JivRbyMHrsH',
+  database: 'bc3fesmygiz7qvs23',
+  port: 3306
+};
