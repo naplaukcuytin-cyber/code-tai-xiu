341f435dd526089503cb603bccd985b44c0a2f46
naplaukcuytin-cyber <phongconcac207@gmail.com>
2026-08-15 10:10:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,21 @@
 /*
 	Tac gia : TRAN DO DUC NGHIA
 */
-console.clear()
+console.clear();
 process.env.TZ = 'Asia/Ho_Chi_Minh';
-console.log('Thoigian:'+(new Date()).getDate());
+console.log('Thoigian:' + (new Date()).getDate());
 require('dotenv').config();
 var cors = require('cors');
 let express = require('express');
 let app = express();
+
 app.use(cors({  
     origin: '*',
     optionsSuccessStatus: 200
 }));
 
 let port = process.env.PORT || 2004;
-// port = 3000;
+
 let expressWs = require('express-ws')(app);
 let since2004 = expressWs.getWss();
 global['list'] = []; 
@@ -28,29 +29,25 @@ require('./app/Controller/Cron/CSMM')(since2004);
 require('./app/Controller/Cron/TaiXiu')(since2004);
 require('./app/Controller/Cron/Cron');
  
-app.listen(port, function() { 
+// Mở cổng lắng nghe và bind vào '0.0.0.0' cho Render nhận diện
+app.listen(port, '0.0.0.0', function() { 
     console.log("Server listen on port ", port);
 }); 
 
-let mysqli    =     require('./app/Model/mysqli');
+let mysqli = require('./app/Model/mysqli');
 mysqli.query("UPDATE `nguoichoi` SET `time_online` = '0'", function (err, users) {
+    if (err) console.log("DB Notice:", err.message);
 });
 
 console.log('start game');
 
-
 process.on('beforeExit', code => {
-    // Can make asynchronous calls
     setTimeout(() => {
-      console.log(`Process will exit with code: ${code}`)
-      process.exit(code)
-    }, 100)
-  })
+      console.log(`Process will exit with code: ${code}`);
+      process.exit(code);
+    }, 100);
+});
   
-  process.on('exit', code => {
-    // Only synchronous calls
-    console.log(`Process exited with code: ${code}`)
-  })
-  
-  
- 
+process.on('exit', code => {
+    console.log(`Process exited with code: ${code}`);
+});
